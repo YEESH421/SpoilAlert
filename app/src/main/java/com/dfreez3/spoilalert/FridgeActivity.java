0f9c011e8fe5444374f3bcab537569dd352184cc
DidgeridooMH <dpsimpkins@gmail.com>
2018-10-21 23:39:13 -0400

    Changed default app to splash screen. Changed app theme and color scheme to fit the app prototypes. Changed the style to include a full screen design. Added drawable icons for fridge screen. Created the layout for the fridge screen.

--- app/src/main/java/com/dfreez3/spoilalert/FridgeActivity.java
+++ app/src/main/java/com/dfreez3/spoilalert/FridgeActivity.java
@@ -1,13 +1,26 @@
 package com.dfreez3.spoilalert;
 
+import android.app.Activity;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class FridgeActivity extends AppCompatActivity {
+public class FridgeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
+
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_fridge);
     }
 }
