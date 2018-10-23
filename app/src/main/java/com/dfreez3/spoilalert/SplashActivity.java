d105e9a31ab81066d6015c84d16782d38000e7ee
tony zhou <tzhou49@gmail.com>
2018-10-22 20:07:34 -0400

    splash screen update

--- app/src/main/java/com/dfreez3/spoilalert/SplashActivity.java
+++ app/src/main/java/com/dfreez3/spoilalert/SplashActivity.java
@@ -11,6 +11,7 @@ public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
@@ -32,6 +33,6 @@ public class SplashActivity extends AppCompatActivity {
                 //overridePendingTransition(R.transition.fadein, R.transition.fadeout);
                 startActivity(Fridge);
             }
-        }, 2500);
+        }, 0);
     }
 }
